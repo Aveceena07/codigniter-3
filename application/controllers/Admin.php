36d79c7fb72cb484ca9e25d915f43b25bb3e119e
Aveceena07 <mingave11@gmail.com>
2023-10-07 09:23:25 +0700

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -487,5 +487,26 @@ class Admin extends CI_Controller
             echo 'Invalid File';
         }
     }
+
+    public function guru()
+    {
+        $data['guru'] = $this->m_model->get_data('guru')->result();
+        $data['kelas'] = $this->m_model->get_data('kelas')->result();
+        $this->load->view('admin/guru', $data);
+    }
+
+    public function export_guru()
+    {
+        $data['data_guru'] = $this->m_model->get_data('guru')->result();
+        $data['nama'] = 'guru';
+        if ($this->uri->segment(3) == 'pdf') {
+            $this->load->library('pdf');
+            $this->pdf->load_view('admin/export_data_guru', $data);
+            $this->pdf->render();
+            $this->pdf->stream('data_guru.pdf', ['Attachment' => false]);
+        } else {
+            $this->load->view('admin/download_data_guru', $data);
+        }
+    }
 }
 ?>
